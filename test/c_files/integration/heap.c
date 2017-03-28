c00133b6f01c08731852a326ecca760306f21107
Lorenzo Silvestri <code@silvestrix.org>
2017-03-28 18:14:16 +0100

    added heap test

---
+++ test/c_files/integration/heap.c
@@ -0,0 +1,27 @@
+
+#include "../framework/integration_driver.h"
+
+typedef struct {
+	int a, b;
+} pair;
+
+int main() {
+	int n, i;
+	pair *arr;
+
+	printf("Input the number of structures to allocate on the stack: ");
+	scanf("%d", &n);
+
+	printf("allocating %d bytes on heap\n", n * sizeof(pair));
+
+	arr = malloc(n * sizeof(pair));
+
+	printf("arr points to %p\n", arr);
+
+	for(i = 0; i < n; ++i) {
+		(*(arr + i)).a = random_get();
+		printf("i = %d:  %d, %d\n", i, (*(arr + i)).a, (*(arr + i)).b);
+	}
+
+	return 0;
+}
